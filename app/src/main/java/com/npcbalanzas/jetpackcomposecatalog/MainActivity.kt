73d03f70bfb0bad947b80ec98bfad4a9b0fc7e64
JeffersonCabello <jefcsanc@espol.edu.ec>
2025-02-03 22:11:53 -0500

    timeforce

--- app/src/main/java/com/npcbalanzas/jetpackcomposecatalog/MainActivity.kt
+++ app/src/main/java/com/npcbalanzas/jetpackcomposecatalog/MainActivity.kt
@@ -6,22 +6,35 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Star
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -39,7 +52,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             JetPackComposeCatalogTheme {
-                MyButtonExample()
+                MyIcon()
             }
         }
     }
@@ -49,69 +62,130 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun DefaultPreview() {
     JetPackComposeCatalogTheme {
-        MyButtonExample()
+        MyIcon()
     }
 }
 
 @Composable
-fun MyButtonExample(){
-    Column(modifier = Modifier.fillMaxSize().padding(24.dp)) {
-        Button(onClick = {
+fun MyIcon(){
+    Icon(imageVector = Icons.Rounded.Star,
+        contentDescription = "CC",
+        tint = Color.Green)
+}
 
-        }, colors = ButtonDefaults.buttonColors(
+@Composable
+fun MyImageAdvance(){
+    Image(
+        modifier = Modifier.clip(shape = CircleShape).border(5.dp,Color.Red, shape = CircleShape),
+        contentDescription = "Ejercicio",
+        painter = painterResource(id = R.drawable.ic_launcher_background)
+    )
+}
+
+@Composable
+fun MyImage() {
+
+}
+
+@Composable
+fun MyButtonExample() {
+
+    var enabled by rememberSaveable { mutableStateOf(true) };
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp)
+    ) {
+        Button(colors = ButtonDefaults.buttonColors(
             containerColor = Color.Magenta,
             contentColor = Color.White
-        ), border = BorderStroke(1.dp,Color.White)
+        ), enabled = enabled,
+            border = BorderStroke(1.dp, Color.White),
+            onClick = {
+                enabled = false
+            }) {
+            Text(text = "Hola")
+        }
+
+        OutlinedButton(
+            colors = ButtonDefaults.buttonColors(
+                disabledContainerColor = Color.Red,
+                contentColor = Color.Black,
+                containerColor = Color.Magenta
+            ),
+            enabled = enabled, modifier = Modifier.padding(top = 8.dp),
+            onClick = {
+
+            },
         ) {
+            Text(text = "Hola")
+        }
+
+        TextButton(onClick = {}) {
             Text(text = "Hola")
         }
     }
 }
 
 @Composable
-fun MyTextFieldOutlined(){
+fun MyTextFieldOutlined() {
     var myText by remember { mutableStateOf("") }
-    OutlinedTextField(value = myText, onValueChange = {myText = it}, modifier = Modifier.padding(all = 4.dp),
-        label = { Text( text = "Holita", textAlign = TextAlign.Center) },
-        colors = TextFieldDefaults.colors(focusedPlaceholderColor = Color.Magenta,
-            unfocusedPlaceholderColor = Color.Blue))
+    OutlinedTextField(
+        value = myText, onValueChange = { myText = it }, modifier = Modifier.padding(all = 4.dp),
+        label = { Text(text = "Holita", textAlign = TextAlign.Center) },
+        colors = TextFieldDefaults.colors(
+            focusedPlaceholderColor = Color.Magenta,
+            unfocusedPlaceholderColor = Color.Blue
+        )
+    )
 }
 
 @Composable
-fun MyTextFieldAdvance(){
+fun MyTextFieldAdvance() {
     var myText by remember { mutableStateOf("") }
     TextField(value = myText,
         onValueChange = {
-            myText = if(it.contains("a")){
-                it.replace("a","")
-            }else{
+            myText = if (it.contains("a")) {
+                it.replace("a", "")
+            } else {
                 it
             }
-                        },
+        },
         label = {
-        Text("Introduce tu nombre")
-    })
+            Text("Introduce tu nombre")
+        })
 
 }
 
 @Composable
-fun MyTextField(name:String, onValueChanged:(String)-> Unit){
+fun MyTextField(name: String, onValueChanged: (String) -> Unit) {
     TextField(value = name, onValueChange = onValueChanged)
 }
 
 @Composable
-fun MyText(){
+fun MyText() {
     Column(modifier = Modifier.fillMaxSize()) {
         Text(text = "Esto es un ejemplo")
         Text(text = "Esto es un ejemplo", color = Color.Blue)
         Text(text = "Esto es un ejemplo", fontWeight = FontWeight.Bold)
         Text(text = "Esto es un ejemplo", fontWeight = FontWeight.Light)
         Text(text = "Esto es un ejemplo", style = TextStyle(fontFamily = FontFamily.Cursive))
-        Text(text = "Esto es un ejemplo", style = TextStyle(textDecoration = TextDecoration.LineThrough))
-        Text(text = "Esto es un ejemplo", style = TextStyle(textDecoration = TextDecoration.Underline))
-        Text(text = "Esto es un ejemplo", style = TextStyle(textDecoration = TextDecoration.combine(
-            listOf(TextDecoration.Underline, TextDecoration.LineThrough)
-        )))
+        Text(
+            text = "Esto es un ejemplo",
+            style = TextStyle(textDecoration = TextDecoration.LineThrough)
+        )
+        Text(
+            text = "Esto es un ejemplo",
+            style = TextStyle(textDecoration = TextDecoration.Underline)
+        )
+        Text(
+            text = "Esto es un ejemplo", style = TextStyle(
+                textDecoration = TextDecoration.combine(
+                    listOf(TextDecoration.Underline, TextDecoration.LineThrough)
+                )
+            )
+        )
         Text(text = "Esto es un ejemplo", fontSize = 30.sp)
     }
 }
